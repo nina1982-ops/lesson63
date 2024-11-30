1eee0525c6be66696ef5a141fd1200efe4326f6a
nina1982-ops <ya.nina.82@yandex.ru>
2024-11-30 18:29:50 +0300

    Add files via upload

--- module_13_5.py
+++ module_13_5.py
@@ -1,29 +1,3 @@
-# from aiogram import Bot, Dispatcher, executor, types
-# from aiogram.contrib.fsm_storage.memory import MemoryStorage
-# from aiogram.dispatcher.filters.state import State, StatesGroup
-# from aiogram.dispatcher import FSMContext
-# from  aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-# import asyncio
-#
-# api = '8143181263:AAHYq9di-YxBuZlHf1bP_ddnqmGky_2rovI'
-# bot = Bot(token=api)
-# dp = Dispatcher(bot, storage=MemoryStorage())
-#
-# kb = ReplyKeyboardMarkup()
-# button_1 = KeyboardButton(text='Информация')
-# button_2 = KeyboardButton(text='Начало')
-# kb.add(button_1)
-# kb.add(button_2)
-#
-# @dp.message_handler(commands=['start'])
-# async def start(message):
-#     await message.answer('Привет!', reply_markup=kb)
-#
-# @dp.message_handler(text='Информация')
-# async def inform(message):
-#     await message.answer('Информация о боте')
-
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
